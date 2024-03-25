7056eea8515fcced966ae50bafc3e82aeda3617e
Lenovo <manasesmutembei11@gmail.com>
2024-03-25 11:43:20 +0300

    added context class

---
+++ Company.Infrastructure/Data/CompanyContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Core.Models.Blog;
+using Company.Core.Models.Commerce;
+using Company.Core.Models.UserInfo;
+
+namespace Company.Infrastructure.Data
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Article>? Articles { get; set; }
+        public DbSet<Author>? Authors { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }    
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ShippingAddress> ShippingAddresses { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+
+
+    }
+}
