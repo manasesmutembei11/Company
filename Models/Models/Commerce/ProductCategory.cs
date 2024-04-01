be25cfc9e27f380cd022735b77aa76af9d5e3811
Lenovo <manasesmutembei11@gmail.com>
2024-04-01 21:38:59 +0300

    ok

--- Models/Models/Commerce/ProductCategory.cs
+++ Models/Models/Commerce/ProductCategory.cs
@@ -11,7 +11,7 @@ namespace Company.Core.Models.Commerce
         public Guid ProductCategoryId { get; set; }
 
         public int ProductCategoryCode { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public string? Description { get; set; }
 
